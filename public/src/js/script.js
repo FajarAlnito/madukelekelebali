9ac68fb03a674704fb6adc6d744dfaa6bd0e5ef3
FajarAlnito <fajaralnito18@gmail.com>
2020-05-23 09:21:11 +0800

    initial commit

---
+++ public/src/js/script.js
@@ -0,0 +1,29 @@
+$(window).scroll(()=>{
+    let height = $(".to-top").height();
+    $('.to-top').toggleClass('fadeIn', $(this).scrollTop() > height - 1);
+    $('.to-top').toggleClass('fadeOut', $(this).scrollTop() < height - 1);
+});
+$(document).ready(()=>{
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+            scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+            });
+        } // End if
+    });
+});
